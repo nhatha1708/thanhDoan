1488170576f2812d568c9ba7633423c4fa639cba
Quoc Huy <trancongquochuy123@gmail.com>
2024-04-21 14:03:37 +0700

    update code

--- main.py
+++ main.py
@@ -2,31 +2,29 @@ from thanhDoan.thanhDoan import ThanhDoan
 import schedule
 import time 
 
-
 try:
     with ThanhDoan() as bot:
         bot.land_first_page()
         data = bot.get_data()
-        new_df = data.head(20)
-        bot.to_csv(new_df)
+        bot.to_csv(data)
 except Exception as e:
-    print(e)
-    print("There is a problem running this program from the command line interface")
+    print(f"An error occurred: {str(e)}")
+
 # def run_bot():
 #     try:
 #         with ThanhDoan() as bot:
 #             bot.land_first_page()
 #             data = bot.get_data()
-#             new_df = bot.new_df(data)
-#             bot.to_csv(new_df)
+#             bot.to_csv(data)
 #     except Exception as e:
-#         print(e)
+#         print(f"An error occurred: {str(e)}")
 #         print("There is a problem running this program from the command line interface")
+
 
 # schedule.every().day.at("00:00").do(run_bot)
 
 # while True:
-#     print("Waiting for")
-#     time.sleep(1300)
+#     print("Waiting for scheduled time...")
 #     schedule.run_pending()
+#     time.sleep(60)
     
--- thanhDoan/thanhDoan.py
+++ thanhDoan/thanhDoan.py
@@ -8,50 +8,47 @@ import pandas as pd
 import os
 from datetime import datetime
 
-
-
 def clean_text(text):
-            my_string = ", ".join(text)
-            index = my_string.find('\nBÀI VIẾT LIÊN QUAN')
-            my_string = my_string[:index]
-            # Split the text into lines
-            lines = my_string.split('\n')
-            
-            # Remove unwanted lines and phrases
-            cleaned_lines = [line for line in lines]
-            
-            # Join the cleaned lines back into a single string
-            cleaned_text = '\n'.join(cleaned_lines)
-            return cleaned_text
+    my_string = ", ".join(text)
+    index = my_string.find('\nBÀI VIẾT LIÊN QUAN')
+    my_string = my_string[:index]
+    # Split the text into lines
+    lines = my_string.split('\n')
+
+    # Remove unwanted lines and phrases
+    cleaned_lines = [line for line in lines]
+
+    # Join the cleaned lines back into a single string
+    cleaned_text = '\n'.join(cleaned_lines)
+    return cleaned_text
 
 def convert_to_datetime(date_str):
-        # Tạo từ điển ánh xạ tên tháng tiếng Việt sang tiếng Anh
-        month_mapping = {
-            'Tháng Một,': 'January',
-            'Tháng Hai,': 'February',
-            'Tháng Ba,': 'March',
-            'Tháng Tư,': 'April',
-            'Tháng Năm,': 'May',
-            'Tháng Sáu,': 'June',
-            'Tháng Bảy,': 'July',
-            'Tháng Tám,': 'August',
-            'Tháng Chín,': 'September',
-            'Tháng Mười,': 'October',
-            'Tháng Mười Một,': 'November',
-            'Tháng Mười Hai,': 'December'
-        }
-        
-        # Thay thế tên tháng tiếng Việt bằng tên tháng tiếng Anh tương ứng
-        for viet_month, eng_month in month_mapping.items():
-            if viet_month in date_str:
-                date_str = date_str.replace(viet_month, eng_month)
-                break
-        
-        # Chuyển chuỗi ngày tháng sang đối tượng datetime
-        return datetime.strptime(date_str, "'%d %B %Y'")
+    # Tạo từ điển ánh xạ tên tháng tiếng Việt sang tiếng Anh
+    month_mapping = {
+        'Tháng Một,': 'January',
+        'Tháng Hai,': 'February',
+        'Tháng Ba,': 'March',
+        'Tháng Tư,': 'April',
+        'Tháng Năm,': 'May',
+        'Tháng Sáu,': 'June',
+        'Tháng Bảy,': 'July',
+        'Tháng Tám,': 'August',
+        'Tháng Chín,': 'September',
+        'Tháng Mười,': 'October',
+        'Tháng Mười Một,': 'November',
+        'Tháng Mười Hai,': 'December'
+    }
+
+    # Thay thế tên tháng tiếng Việt bằng tên tháng tiếng Anh tương ứng
+    for viet_month, eng_month in month_mapping.items():
+        if viet_month in date_str:
+            date_str = date_str.replace(viet_month, eng_month)
+            break
+
+    # Chuyển chuỗi ngày tháng sang đối tượng datetime
+    return datetime.strptime(date_str, "'%d %B %Y'")
 
 class ThanhDoan(webdriver.Chrome):
-# luu tru nhung duong dan cua driver
     def __init__(self, driver_path=r"C:\Users\admin\OneDrive\Tài liệu\Python Scripts\thanhDoan",
                  teardown=False):
         self.driver_path = driver_path
@@ -61,7 +58,7 @@ class ThanhDoan(webdriver.Chrome):
         self.implicitly_wait(150)
         self.maximize_window()
 
-    def __exit__(self,  exc_type, exc_value, exc_tb):
+    def __exit__(self, exc_type, exc_value, exc_tb):
         if self.teardown:
             self.quit()
         pass
@@ -70,19 +67,15 @@ class ThanhDoan(webdriver.Chrome):
         self.get('http://thanhdoandanang.org.vn/')
         self.implicitly_wait(15)
 
-    
     def get_data(self):
-
-        # get title and link href
-        titles, links = [],[]
-        title = self.find_elements(By.CSS_SELECTOR, ".tab-content, .entry-title [href]" )# Extract text and href attributes
+        titles, links = [], []
+        title = self.find_elements(By.CSS_SELECTOR, ".tab-content, .entry-title [href]")
         titles = [t.get_attribute('title') for t in title]
         links = [t.get_attribute('href') for t in title]
 
-
-        list1 = [None]  * len(links)
-        list2 = [None]  * len(links)
-        for i in range(5):
+        list1 = [None] * len(links)
+        list2 = [None] * len(links)
+        for i in range(len(links)):
             try:
                 self.get(links[i])
                 self.implicitly_wait(15)
@@ -94,34 +87,27 @@ class ThanhDoan(webdriver.Chrome):
                 d2 = [content.text for content in content_list]
                 cleaned_text = clean_text(d2)
                 list2[i] = cleaned_text
-                pass
             except Exception as e:
-                print(f"index {i}: " + e.message)
-                pass
+                print(f"Error at index {i}: {str(e)}")
 
-
-        df = []
         df = pd.DataFrame({
-                'title': titles,
-                'link': links,
-                'date': list1,
-                'content': list2})
-        
+            'title': titles,
+            'link': links,
+            'date': list1,
+            'content': list2})
 
         df = df.dropna()
         df['date'] = df['date'].astype(str)
         df['date'] = [d.replace('[', '').replace(']', '') for d in df['date']]
         df['date'] = df['date'].apply(convert_to_datetime)
-        df.sort_values(by='date', inplace=True, ascending = False) 
+        df.sort_values(by='date', inplace=True, ascending=False)
 
-        df['Index'] = range(1,len(df)+1)
-        comlumn_order = ['Index']+[col for col in df if col != 'Index']
-        df = df[comlumn_order]
+        df['Index'] = range(1, len(df) + 1)
+        column_order = ['Index'] + [col for col in df if col != 'Index']
+        df = df[column_order]
 
         return df
 
-    def to_csv(self,df):  
+    def to_csv(self, df):
         data = df.to_csv('dataframe.csv', index=False)
         return data
-        
-
