724a207971c8fe521ab4410fa0eff1ca59eb7c6f
Quoc Huy <trancongquochuy123@gmail.com>
2024-05-13 16:38:25 +0700

    update

--- fb.py
+++ fb.py
@@ -21,7 +21,7 @@ post_urls = [None]*40
 date = [None]*40
 list_content = [None]*40
 for post in get_posts('hoisinhviendanang', base_url="https://mbasic.facebook.com", start_url="https://mbasic.facebook.com/hoisinhviendanang?v=timeline&lst=100025055373500%3A100064556115412%3A1715361298&eav=AfYEcgIK-dATP-jj-eqtrax6PwWy0KrhYBJBGjLoZCfhO1yp8I0tBQKO5bKZmEzGdZA&paipv=0", pages=10, cookies=cookies):
-        time.sleep(10)
+        time.sleep(200)
         content = [None]
         print(index)
         date[index] = post['time']
@@ -40,7 +40,7 @@ with open('./hoithieunhiHeaders.json', 'r') as file:
     _scraper.mbasic_headers = json.load(file)
 
 for post in get_posts('Thieunhithanhphodanang', base_url="https://mbasic.facebook.com", start_url="https://mbasic.facebook.com/Thieunhithanhphodanang?v=timeline&lst=100025055373500%3A100050942934157%3A1715368776&eav=AfZ4xlSOtfhRJiHfIctWakILlqxgIa7iRafrtGEr5_fkfUAngRHNlEzXht_n6pEjb0g&paipv=0", pages=10, cookies=cookies):
-        time.sleep(10)
+        time.sleep(200)
         content = [None]
         print(index)
         date[index] = post['time']
@@ -58,7 +58,7 @@ with open('./tuoitrednHeaders.json', 'r') as file:
     _scraper.mbasic_headers = json.load(file)
 
 for post in get_posts('tuoitredanangdn', base_url="https://mbasic.facebook.com", start_url="https://mbasic.facebook.com/tuoitredanangdn?v=timeline&lst=100025055373500%3A100064539736066%3A1715408567&eav=AfbS6dJYEfoiU84j16b7t-qAKEBfyRdKzWO3Tgi-saPQcL9S7S-GCSBjtnrXz1FL954&paipv=0", pages=10, cookies=cookies):
-        time.sleep(10)
+        time.sleep(200)
         content = [None]
         print(index)
         date[index] = post['time']
@@ -77,7 +77,7 @@ with open('./vieclamtndnHeaders.json', 'r') as file:
     _scraper.mbasic_headers = json.load(file)
 
 for post in get_posts('vieclamthanhniendanang', base_url="https://mbasic.facebook.com", start_url="https://mbasic.facebook.com/vieclamthanhniendanang?v=timeline&lst=100025055373500%3A100090460894592%3A1715408837&eav=AfbOHuRS5YCENXUPK3BKWFm4rXRcZiAoKIDEGmRYnsKRDovr8-tg0u4E__ujxGfuzfE&paipv=0", pages=10, cookies=cookies):
-        time.sleep(400)
+        time.sleep(300)
         content = [None]
         print(index)
         date[index] = post['time']
@@ -88,7 +88,7 @@ for post in get_posts('vieclamthanhniendanang', base_url="https://mbasic.faceboo
         else:
                 list_content[index] = "None found"
         index = index + 1
-        if index == 3:
+        if index == 40:
                 break
 
 # Tao df va xuat csv
